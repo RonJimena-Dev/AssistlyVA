18c8773e4f5a5a2ef684f3fe44e50e83d482d9b9
Ronald Jimena <ronald.jimena0420@gmail.com>
2025-07-26 16:10:00 +0800

    adding faviconv2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${poppins.variable} scroll-smooth`}>
       <head>
+        {/* Standard favicon */}
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        {/* SVG favicon for modern browsers */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        {/* PNG favicons for various sizes */}
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/web-app-manifest-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/web-app-manifest-512x512.png" />
+        {/* Apple Touch Icon for iOS */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        {/* Web App Manifest */}
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className="font-sans bg-gray-50">
         <Navigation />
